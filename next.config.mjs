ddc6a0a70cb0d0d7f0433662686b97bdeb15a2a0
gohar514 <goharhassanmustafa@gmail.com>
2024-11-28 01:45:45 +0500

    sharp dp istall and next config change

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,25 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-       
-      domains: ['img.freepik.com','images.pexels.com', 'corio.pk' ], // Add the external domains you're using here
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'corio.pk',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
